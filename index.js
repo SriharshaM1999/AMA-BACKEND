09c7375c5dd1e75b7ec0f1f81930f42b050574a3
SriharshaM1999 <sriharshamadireddy@gmail.com>
2021-07-18 19:22:26 +0530

    work more on errors

--- index.js
+++ index.js
@@ -26,27 +26,26 @@ app.use(express.urlencoded());
 app.use(express.json());
 
 
-app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+// app.use(function (req, res, next) {
+//     // Website you wish to allow to connect
+//     // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
-    res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'POST');
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'POST');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
-    next();
-});
+//     // Pass to next layer of middleware
+//     next();
+// });
 
-app.use(cors());
 
 app.use('/', router);
 
